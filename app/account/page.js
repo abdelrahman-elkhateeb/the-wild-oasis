eb2a232739b56621b8862dca289aee363325d4cb
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-07-18 15:49:39 +0300

    add select country component

--- app/account/page.js
+++ app/account/page.js
@@ -3,5 +3,7 @@ export const metadata = {
 }
 
 export default function Account() {
-  return <h1>account page</h1>
+  return <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+    welcome, abdelrahman elkhateeb
+  </h2>
 };

84de159f43b09b667c46c8b2eea112b98091e73a
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-08-01 19:38:09 +0300

    Update Filter.js

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -6,7 +6,7 @@ function Filter() {
       <button className="px-5 py-2 hover:bg-primary-700">All cabins</button>
       <button className="px-5 py-2 hover:bg-primary-700">1&mdash;3</button>
       <button className="px-5 py-2 hover:bg-primary-700">4&mdash;8</button>
-      <button className="px-5 py-2 hover:bg-primary-700">&mdash;12</button>
+      <button className="px-5 py-2 hover:bg-primary-700">8&mdash;12</button>
     </div>
   )
 }

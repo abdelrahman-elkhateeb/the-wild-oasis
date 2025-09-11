36080d11166463979c22bb46226481cf56ace131
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-09-11 13:19:21 +0300

    building custom sign in page

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,14 @@
+import { auth } from "../_lib/auth"
+
 export const metadata = {
-  title: "Account"
+  title: "Guest Area"
 }
 
-export default function Account() {
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+
   return <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-    welcome, abdelrahman elkhateeb
+    welcome, {firstName}
   </h2>
 };

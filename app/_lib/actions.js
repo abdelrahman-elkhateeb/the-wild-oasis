2b16ffc6521ac67dfb10f6638e70f6ea57484acc
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2025-09-20 15:46:00 +0300

    update the profile using a server action

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,31 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) throw new Error("you must be logged in");
+
+  const id = session.user.guestId;
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[A-Za-z0-9]{6,12}$/.test(nationalID)) throw new Error("please provide a valid national ID");
+
+  const updateData = { nationality, countryFlag, nationalID };
+
+  const { data, error } = await supabase
+    .from('guests')
+    .update(updateData)
+    .eq('id', id)
+
+  if (error) {
+    console.error(error);
+    throw new Error('Guest could not be updated');
+  }
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
